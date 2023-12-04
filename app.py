db23d490438a085f8a47839a4f586fe927503237
dtshering76 <108328481+dtshering76@users.noreply.github.com>
2023-12-04 06:00:31 +0000

    Added API

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
+jobs=[
+  {'id':1,
+   'location':'Thimphu',
+   'title':"Software Engineer",
+   'company':"Google",
+   'salary':"Nu.100000"
+  },
+  {'id':2,
+   'location':'Phuentsholing',
+   'title':"Software Engineer",
+   'company':"Facebook",
+   'salary':"Nu.200000"
+  },
+  {'id':3,
+   'location':'Paro',
+   'title':"Software Engineer",
+   'company':"Tweeter",
+   'salary':"Nu.150000"
+  },
+  {'id':4,
+   'location':'Trashigang',
+   'title':"Software Engineer",
+   'company':"Techpark",
+   
+  },
+  
+]
+
 @app.route('/')
 def index():
-  return render_template('home.html')
+  return render_template('home.html', jobs=jobs)
+
+@app.route('/jobs')
+def job_list():
+  return jsonify(jobs)
 
 
 
